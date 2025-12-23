31e850b9c15fa07c9d7cb40f7adad7eec0ef296f
baksoum <99664268+baksoum@users.noreply.github.com>
2025-12-23 18:28:32 -0500

    Add variables for Azure configuration

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Virtual machine name"
+  type        = string
+}
